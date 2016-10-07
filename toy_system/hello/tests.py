e08a101e1add0585e1433413fc2f3e7b41f69185
Christopher Conte <chris@conte.org>
2016-10-07 01:29:08 -0400

    tests

--- toy_system/hello/tests.py
+++ toy_system/hello/tests.py
@@ -1,3 +1,34 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from .views import *
+from .models import *
 
 # Create your tests here.
+
+class HelloTests(TestCase):
+
+    def setUp(self):
+        Person.objects.create(person_name="lion", person_bio="roar")
+        Person.objects.create(person_name="cat", person_bio="meow")
+
+    def test_nameTest(self):
+        p = get_object_or_404(Person, pk=1)
+
+        self.assertIs((str("lion") ==  str(p.person_name)), True)
+
+    def test_nameChange(self):
+        class request(object):
+            POST = {'name': 'name_test', 'bio':'bio_test'}
+            def __init__(self):
+                self.POST['name'] = 'name_test'
+
+
+        post = request()
+        edit(post, 1)
+        
+        p = get_object_or_404(Person, pk=1)
+        edit(post, 1)
+        
+        self.assertIs(str(p.person_name) == post.POST['name'], True)
+        
+
--- toy_system/hello/views.py
+++ toy_system/hello/views.py
@@ -10,23 +10,22 @@ from .models import Person
 def detail(request, person_id):
     person = Person.objects.get(id=person_id)
     context = {
-	'person': person,
+    'person': person,
     }
     return render(request,'hello/detail.html',context)
 
 def index(request):
     team_members = Person.objects.all()
     context = {
-	'team_members': team_members,
+    'team_members': team_members,
     }
     return render(request,'hello/index.html',context)
 
 def edit(request, person_id):
     person = get_object_or_404(Person, pk=person_id)
-    
-    person.person_name = request.POST['name']
-    person.person_bio = request.POST['bio']
+    print person.person_name
     person.save()
+    print person.person_name
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
