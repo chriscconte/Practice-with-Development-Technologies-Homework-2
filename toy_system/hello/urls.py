1871b29a44029cd6af2ea0ede2209addf4c0c4b4
Christopher Conte <chris@conte.org>
2016-10-06 20:06:30 -0400

    post logic

--- toy_system/hello/urls.py
+++ toy_system/hello/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^(?P<person_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<person_id>[0-9]+)/edit/$', views.edit, name='edit'),
 ]
--- toy_system/hello/views.py
+++ toy_system/hello/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import textwrap
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 from django.views.generic.base import View
 
@@ -19,3 +20,24 @@ def index(request):
 	'team_members': team_members,
     }
     return render(request,'hello/index.html',context)
+
+def edit(request, person_id):
+    person = get_object_or_404(Person, pk=person_id)
+    print (request.POST) 
+    try:
+        name = request.POST['name']
+        bio = request.POST['bio'] 
+    except (KeyError, person.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'hello/detail.html', {
+            'person': person,
+            'error-message': 'oops something went wrong',
+        })
+    else:
+        person.person_name = name
+        person.person_bio = bio
+        person.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('detail', args=(person.id,)))
